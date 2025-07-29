03e19bb9cd2504409cc0fd13709fe819f1dd7945
Precious <lamberville73@gmail.com>
2025-07-29 15:13:27 +0100

    Added HeaderSection component with introductory message

---
+++ src/app/components/HeaderSection.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const HeaderSection = () => {
+  return (
+    <section>
+      <h1 className="text-white">
+        {" "}
+        Hello, I am Precious, I love building and creating stuff{" "}
+      </h1>
+    </section>
+  );
+};
+
+export default HeaderSection;

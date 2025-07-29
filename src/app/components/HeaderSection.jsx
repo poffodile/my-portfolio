4ff23ef0fd8ead324e064993ec5cb48fe8a75774
Precious <lamberville73@gmail.com>
2025-07-29 15:53:26 +0100

    Implementedcode changes to enhance functionality and improve performance

--- src/app/components/HeaderSection.jsx
+++ src/app/components/HeaderSection.jsx
@@ -3,10 +3,17 @@ import React from "react";
 const HeaderSection = () => {
   return (
     <section>
-      <h1 className="text-white">
-        {" "}
-        Hello, I am Precious, I love building and creating stuff{" "}
-      </h1>
+      <div className="grid  grid-cols-1 lg:grid-cols-12">
+        <div className=" col-span-7 place-self-center">
+          <h1 className="text-white mb-4 text-4xl lg:text-6xl font-extrabold">
+            Hello, I am Precious, I love building and creating stuff
+          </h1>
+          <p className="text-[#ADB7BE] text-lg lg:text-xl">
+            This is going to be a bunch of text describing my projects and
+            skills.
+          </p>
+        </div>
+      </div>
     </section>
   );
 };
--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,10 @@
 import Image from "next/image";
+import HeaderSection from "./components/HeaderSection";
 
 export default function Home() {
-  return <main className="flex min-h-screen flex-col bg-[#121212] "></main>;
+  return (
+    <main className="flex min-h-screen flex-col bg-[#121212] container mx-auto ">
+      <HeaderSection />
+    </main>
+  );
 }

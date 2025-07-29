885946e3258a8e330329fac6c385faa81ff3d71a
Precious <lamberville73@gmail.com>
2025-07-29 20:45:55 +0100

    Refactor code structure for improved readability and maintainability

--- src/app/components/HeaderSection.jsx
+++ src/app/components/HeaderSection.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import Image from "next/image";
 
 const HeaderSection = () => {
   return (
@@ -8,10 +9,29 @@ const HeaderSection = () => {
           <h1 className="text-white mb-4 text-4xl lg:text-6xl font-extrabold">
             Hello, I am Precious, I love building and creating stuff
           </h1>
-          <p className="text-[#ADB7BE] text-lg lg:text-xl">
+          <p className="text-[#ADB7BE] text-lg mb-6 lg:text-xl">
             This is going to be a bunch of text describing my projects and
             skills.
           </p>
+          <div>
+            <button className="px-6 py-3 rounded-full mr-4 bg-white hover:bg-slate-200 text-black">
+              Hire Me
+            </button>
+            <button className="px-6 py-3 rounded-full bg-transparent hover: bg-slate-800 text-white border border-white mt-3  mr-4">
+              Download CV
+            </button>
+          </div>
+        </div>
+        <div className="col-span-5 place-self-center mt-4 lg:mt-0">
+          <div className="rounded-full bg-[#181818] w-[250px] h-[250px]  lg:w-[400px] relative">
+            <Image
+              src="/images/headerimage.jpg"
+              alt="Profile Picture"
+              className="absolute transform-translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2 rounded-full"
+              width={300}
+              height={300}
+            />
+          </div>
         </div>
       </div>
     </section>
--- src/app/page.js
+++ src/app/page.js
@@ -3,8 +3,10 @@ import HeaderSection from "./components/HeaderSection";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col bg-[#121212] container mx-auto ">
-      <HeaderSection />
+    <main className="flex min-h-screen flex-col bg-[#121212] ">
+      <div className="container mx-auto px-12 py-4 ">
+        <HeaderSection />
+      </div>
     </main>
   );
 }

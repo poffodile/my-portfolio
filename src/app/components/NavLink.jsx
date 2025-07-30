73842997de86929fb829d9a9529d62c77a8b8e21
Precious <lamberville73@gmail.com>
2025-07-30 08:57:25 +0100

    Add NavLink component and update Navbar for improved navigation structure; included @heroicons/react dependency

--- src/app/components/NavLink.jsx
+++ src/app/components/NavLink.jsx
@@ -1,9 +1,13 @@
-const NavLink = ({ href, children }) => {
+import Link from "next/link";
+
+const NavLink = ({ href, title }) => {
   return (
     <Link
       href={href}
       className="block py-2 pl-3 pr-4 text-[#ADB7BE] sm:text-xl rounded md:p-0 hover:text-white">
-      {children}
+      {title}
     </Link>
   );
 };
+
+export default NavLink;
